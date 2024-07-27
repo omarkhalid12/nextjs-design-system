57b30b370da61851900b12f77f485c5ae22fdc85
omarkhalid12 <ok3817554@gmail.com>
2024-07-28 01:58:51 +0300

    Pain without Design System

--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -3,11 +3,12 @@ import { ButtonHTMLAttributes, ReactNode } from "react"
 interface IProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   className?: string;
-  width?: "w-full" | "w-fit"
+  width?: "w-full" | "w-fit";
+  
 }
 const Button = ({ children, className, width= "w-full", ...rest } : IProps) => {
   return (
-    <button className={`${className} ${width} rounded-lg text-white px-3 py-3 duration-200 font-medium` }{...rest}>
+    <button className={`${className} ${width} rounded-lg text-white px-3 py-3 duration-200 font-medium border-2 border-white` }{...rest}>
       {children}
     </button>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,14 @@ import Button from "./components/ui/Button";
 export default function Home() {
   return (
     <main className="min-h-screen p-24">
-      <h1 className="text-center text-4xl bg-white rounded-md text-black w-fit p-5 mx-auto font-sans">
-        Design System in Depth
-      </h1>
-      <Button>Solid Button</Button>
+      <div className="space-y-3 border-2 border-indigo-500 p-3">
+        <Button width="w-fit">SOLID</Button>
+        <Button>DESTRUCTIVE</Button>
+        <Button>SECONDARY</Button>
+        <Button>OUTLINE</Button>
+        <Button>CHOST</Button>
+        <Button>LINK</Button>
+      </div>
     </main>
   );
 }
