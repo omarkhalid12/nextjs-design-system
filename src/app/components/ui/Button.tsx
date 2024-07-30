014212b0b3744a1e0355611876ca00c791a1c651
omarkhalid12 <ok3817554@gmail.com>
2024-07-30 03:23:51 +0300

    From Size Variants to Pixel Perfect

--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -2,16 +2,23 @@ import { ButtonHTMLAttributes, ReactNode } from "react"
 import { cva, type VariantProps  } from "class-variance-authority";
 import { tailwindCMerge } from "@/utils";
 
-const buttonVariants = cva("block rounded-md font-semibold duration-200", {
+const buttonVariants = cva("inline-flex items-center rounded-md duration-200", {
   variants: {
     intent: {
       primary: "bg-white text-black dark:bg-white dark:text-black",
       outline: "bg-transparent text-white dark:text-white border border-grey-300",
       destructive: "bg-red-600 text-black dark:bg-red-800 dark:text-white border border-grey-300"
-    }
+    },
+    size: {
+      xs: "px-2 text-xsv h-6",
+      sm: "px-3 text-sm h-8",
+      base: "px-4 text-base h-10",
+      lg: "px-6 text-lg h-12",
+    },
   },
   defaultVariants: {
     intent: "primary",
+    size: "base",
   }
 })
 
